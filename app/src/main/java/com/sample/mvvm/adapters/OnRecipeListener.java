a8c4ffe4360d0fe8d9a80e7a5b93758d4b6cc173
SawThanDar18 <sawthandar1998.pw@gmail.com>
2021-11-03 20:38:07 +0630

    project created

---
+++ app/src/main/java/com/sample/mvvm/adapters/OnRecipeListener.java
@@ -0,0 +1,8 @@
+package com.sample.mvvm.adapters;
+
+public interface OnRecipeListener {
+
+    void onRecipeClick(int position);
+
+    void onCategoryClick(String category);
+}
---
+++ app/src/main/java/com/sample/mvvm/util/Testing.java
@@ -0,0 +1,16 @@
+package com.sample.mvvm.util;
+
+import android.util.Log;
+
+import com.sample.mvvm.models.Recipe;
+
+import java.util.List;
+
+public class Testing {
+
+    public static void printRecipes(List<Recipe>list, String tag){
+        for(Recipe recipe: list){
+            Log.d(tag, "onChanged: " + recipe.getTitle());
+        }
+    }
+}
